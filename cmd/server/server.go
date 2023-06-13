e66eb15841f0a553b3a8f2cb8d19e138ce613e45
matheusses <matheus.s.es@gmail.com>
2023-06-12 22:08:02 -0300

    chore: crud with graphql

---
+++ cmd/server/server.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/matheusses/go-graphql/graph"
+	"github.com/matheusses/go-graphql/graph/generated"
+	"github.com/matheusses/go-graphql/internal/database"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		log.Fatal("failed to open database: %v", err)
+	}
+	defer db.Close()
+
+	categoryDB := database.NewCategory(db)
+	courseDB := database.NewCourse(db)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{
+		CategoryDB: categoryDB,
+		CourseDB:   courseDB,
+	}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
